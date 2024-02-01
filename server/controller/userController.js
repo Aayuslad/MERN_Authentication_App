fc1728e35bbeb89b25e4f5be6a6af225bf2b714a
aayush <aayushlad058@gmail.com>
2024-02-01 09:30:45 +0530

    CORS & cookie updated

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -69,7 +69,7 @@ export const login = async (req, res) => {
 		// Sending the Authorization token
 		return res
 			.cookie("Authorization", token, {
-				domain: "mern-authentication-app-frontend.vercel.app",
+				// domain: "mern-authentication-app-frontend.vercel.app",
 				httpOnly: true,
 				secure: true,
 				sameSite: "none",
@@ -87,7 +87,7 @@ export const logout = async (req, res) => {
 	try {
 		// Clearing the Authorization cookie
 		res.clearCookie("Authorization", {
-			domain: "mern-authentication-app-frontend.vercel.app",
+			// domain: "mern-authentication-app-frontend.vercel.app",
 			httpOnly: true,
 			secure: true,
 			sameSite: "none",
