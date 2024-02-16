a6194f516268635cb7dd1e90ff74f8e4bb96763d
aayush <aayushlad058@gmail.com>
2024-02-16 09:31:28 +0530

    improved the css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import axios from "axios"
-axios.defaults.baseURL = "https://mern-authentication-app-backend.vercel.app";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+// axios.defaults.baseURL = "https://mern-authentication-app-backend.vercel.app";
+axios.defaults.baseURL = "http://localhost:8080";
 axios.defaults.withCredentials = true
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <GoogleOAuthProvider clientId="<your_client_id>">
+        <App />
+    </GoogleOAuthProvider>
 )
