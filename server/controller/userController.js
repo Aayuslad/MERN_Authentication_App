45290b409d61dd9efc5fb2c4e65b62988ecd85a7
aayush <aayushlad058@gmail.com>
2024-07-05 22:05:38 +0530

    bug fix

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -54,7 +54,7 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
 	const { username, password, token } = req.body;
 
-	console.log(process.env.RECAPTCHA_SECRET_KEY);
+	console.log(req.body);
 
 	const formData = new FormData();
 	formData.append("secret", process.env.RECAPTCHA_SECRET_KEY);
